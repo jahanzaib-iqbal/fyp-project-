99865d314e605f3d118d1dd2656273899758e05f
Hamdan kalyar <hamdan.kalyar@outlook.com>
2023-12-17 06:43:39 +0500

    welcome screen added

--- App.js
+++ App.js
@@ -99,6 +99,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFFF",
+    marginTop:50
     
   },
 });
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button, Image, SafeAreaView } from 'react-native';
-SafeAreaView
+import { useNavigation } from "@react-navigation/native";
+import LargeBtn from '../components/button/LargeBtn';
+
+
 const WelcomeScreen = () => {
+  const navigation = useNavigation();
+  const handleLogin = () => {
+    navigation.navigate("LoginScreen");
+  };
+  const handleSignup = () => {
+    navigation.navigate("SignUpScreen");
+  };
   return (
     
     <View style={styles.container}>
       <Image
-        source={require('../assets/Ballons.jpeg')} 
+        source={require('../assets/Ballons.jpg')} 
         style={styles.backgroundImage}
         resizeMode="contain"
       />
@@ -19,10 +29,21 @@ const WelcomeScreen = () => {
       </View>
       <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Log in" onPress={() => console.log('Log in pressed')} />
+            <LargeBtn 
+            title="Login" 
+            handlePress={handleLogin}
+            backgroundColor='#4e9994'
+            />
+            
           </View>
           <View style={styles.button}>
-            <Button title="Sign up" onPress={() => console.log('Sign up pressed')} color="#f194ff" />
+          <LargeBtn 
+          title="Sign up" 
+          handlePress={handleSignup}
+          backgroundColor='white'
+          border={true}
+          />
+            
           </View>
       </View>
     </View>
@@ -32,40 +53,42 @@ const WelcomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop:40,
+    flex: 1, 
+    paddingTop:15,
     paddingHorizontal:20,
     alignSelf: 'center',
     backgroundColor:'#FCFCFC',
   },
   backgroundImage: {
     width:340,
+    height:450,
     borderTopLeftRadius:20,
     borderTopRightRadius:20,
   },
   textContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    
+    flex: 1,  
+    marginTop:10
   },
   title: {
-    fontSize: 20,
-    fontFamily:'SF-Pro-Text-Heavy',
+    fontSize: 22,
+    fontFamily:'SF-Pro-Text-Bold',
     color:'#3B4948',
     textAlign: 'center',
     marginBottom: 10,
   },
   subtitle: {
-    fontSize: 16,
+    fontSize: 15,
     fontFamily:'SF-Pro-Text-Regular',
     color:'#3B4948',
     textAlign: 'center',
   },
   buttonContainer: {
-    marginBottom: 20, // Adjust based on your screen's design
+    flex:1,
+    marginBottom: 20, 
+    justifyContent:'flex-start'
   },
   button: {
-    marginBottom: 10, // Creates space between the buttons
+   marginBottom: 10, 
   },
 });
 
